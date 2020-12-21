a2085f14cbbfd93d89bbff212684eb52941289e7
Miguel Chiau <chiau.miguel@gmail.com>
2020-12-20 18:16:44 -0800

    skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import {Typography, Avatar, Grid, Box, Paper} from "@material-ui/core"
+import {Typography, Avatar, Grid, Box, Paper, ListItem} from "@material-ui/core"
 import avatar from "../images/prof.jpg"
 import img1 from "../images/html5.png"
 import img2 from "../images/css.png"
-import img3 from "../images/javascript.png"
-import img4 from "../images/node-js.png"
-import img5 from "../images/react (3).png"
-import img6 from "../images/adobe-xd.png"
+import img3 from "../images/sass.png"
+import img4 from "../images/javascript.png"
+import img5 from "../images/node-js.png"
+import img6 from "../images/react (3).png"
+import img7 from "../images/wordpress.png"
+import img8 from "../images/git.png"
+
+
 
 
 
@@ -35,7 +39,6 @@ const useStyles = makeStyles(theme => ({
         textAlign: "center",
         color: theme.palette.text.secondary,
         background: "#fff",
-
         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
     },
     subtitle: {
@@ -63,10 +66,57 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(2),
         textAlign: "center",
         background: "#fff"
-    }
+    },
+    skills: {
+        width: "100%",
+        margin: "0px",
+        background: "#fff",
+        padding: theme.spacing(2),
+        textAlign: "center",
+        // background: "#fff",
+        // boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
+
+
+    },
+    skillsImgs: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+        background: "#fff",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto"
+
+    },
 
 
 }))
+
+const skillsItems = [
+{
+  skillIcon: img1
+},
+{
+  skillIcon: img2
+},
+{
+  skillIcon: img3
+},
+{
+  skillIcon: img4
+},
+{
+  skillIcon: img5
+},
+{
+  skillIcon: img6
+},
+{
+  skillIcon: img7
+},
+{
+  skillIcon: img8
+}
+]
 
 
 const About = () => {
@@ -85,17 +135,16 @@ const About = () => {
             className={classes.grid}
             >
 
-                <Grid item xs={12} md={6} style={{textAlign: "center"}}>
-
+        
+        
+                <Grid item xs={6} md={3} style={{textAlign: "center"}}>
                     <Paper className={classes.profile}>
                     <Avatar className={classes.avatar} variant="round" src={avatar} alt="Miguel Chiau"/>
-
                     </Paper>
                 </Grid>
 
                 <Grid item xs={12} md={6} style={{textAlign: "center"}}>
                     
-                    {/* <Paper className={classes.paper}>2-TWO</Paper> */}
                     <Typography className={classes.title} variant="h6">
                         Hi, I'm Miguel Chiau.
                     </Typography>
@@ -112,20 +161,26 @@ const About = () => {
                       </Typography>
                 </Grid>
 
+
+
                 <Grid item xs={12} md={6} style={{textAlign: "center"}}>
+
                     <Typography  variant="h6">
-                        Experience with:
+                        Skills
                     </Typography>
 
-                    <Avatar src={img1} variant="square" alt=""/>
-                    <Avatar src={img2} variant="square" alt=""/>
-                    <Avatar src={img3} variant="square" alt=""/>
-                    <Avatar src={img4} variant="square" alt=""/>
-                    <Avatar src={img5} variant="square" alt=""/>
-                    <Avatar src={img6} variant="square" alt=""/>
+                    <Grid container spacing={2} className={classes.skills} style={{textAlign: "center"}}>
+                    {
+                    skillsItems.map((lsItem, key) => (
+                        <Grid key={key} item xs={6} md={3} style={{textAlign: "center"}}>
+                        <Paper className={classes.profile}>
+                            <Avatar className={classes.skillsImgs} variant="square" src={lsItem.skillIcon} alt=""/>
+                         </Paper>
+                       </Grid>
+                    ))
+                    }
+                    </Grid>
 
-
-                    
                 </Grid>
                 
 
